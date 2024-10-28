99c7051f7186f26ccf64bbc7a49556e97586d60a
Hugo Vega <hugo17avid@gmail.com>
2024-10-28 08:55:38 -0700

    Create warmup.js

---
+++ warmup.js
@@ -0,0 +1,27 @@
+// let arr = ["hello", "how are you"]
+// function getLengths(arr){
+//     return arr.map(item => item.length)
+// }
+// let lengths = getLengths(arr)
+// console.log(lengths) 
+
+// Problem: Write a function squareArray(arr) that takes an array of numbers and returns a new array with each number squared.
+// arr = [1, 5, 7]
+// function squareArray(arr){
+//     return arr.map(item => item ** 2)
+// }
+// console.log(squareArray(arr))
+
+// Problem: Write a function findMin(arr) that takes an array of numbers and returns the smallest number in the array.
+// arr = [5, 1, 9, 6, 3]
+// function findMin(arr){
+//     arr.sort( (a,b) =>  a - b )
+//     return arr[0]
+// }
+// console.log(findMin(arr))
+
+// Problem: Write a function removeDuplicates(arr) that takes an array and returns a new array with only unique values.
+let arr = [1, 2, 2, 3, 4, 4]
+function removeDuplicates(arr){
+    r = []
+}
